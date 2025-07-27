e836d60fa419a60a03d83ebdce09bfc8ebf2aeab
ShamirAli55 <shamirali9779@gmail.com>
2025-07-27 03:14:23 -0700

    Updated

--- Cyberpunk Theme/main.js
+++ Cyberpunk Theme/main.js
@@ -56,7 +56,7 @@ new RGBELoader()
 
         const loader = new GLTFLoader();
 
-        loader.load('./DamagedHelmet.gltf', (gltf) => {
+        loader.load('public/assets/models/DamagedHelmet/DamagedHelmet.gltf', (gltf) => {
                 // Add the model to the scene
                 model = gltf.scene;
                 scene.add(model);
